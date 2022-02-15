f93d742adf6f03970872e727f931082b1da9186b
poornachander <poornachander0777@gmail.com>
2022-02-15 18:31:29 +0530

    Second Commit by Poornachander

--- firstproject/testapp/views.py
+++ firstproject/testapp/views.py
@@ -4,7 +4,7 @@ import datetime
 
 # Create your views here.
 def welcome(request):
-    s='<h1>Hello Guys This is Poornachander Varukolu.. iam testing weather it is working or not?<br> And this is first django project.</h1>'
+    s='<h1>This is modified project... hence "second commit".<br>Hello Guys This is Poornachander Varukolu.. iam testing weather it is working or not?<br> And this is first django project. Haha..!</h1>'
     p=datetime.datetime.now()
     p=f'<h1>{str(p)}</h1>'
     return HttpResponse(s)
